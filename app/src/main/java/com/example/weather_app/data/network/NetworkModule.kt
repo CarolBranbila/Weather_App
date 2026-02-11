bfada0955e0db0c47a543132a8773cd213a99fda
Carol Branbila <carolbranbila@hotmail.com>
2026-02-11 20:55:47 +0000

    Create NetworkModule

--- app/src/main/java/com/example/weather_app/data/network/NetworkModule.kt
+++ app/src/main/java/com/example/weather_app/data/network/NetworkModule.kt
@@ -1,6 +1,15 @@
 package com.example.weather_app.data.network
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
 
 
+@Module
+@InstallIn(Singleton::class)
 object NetworkModule {
+
+    @Provides
+    fun provideWeatherApi(): WeatherApi = RetrofitInstance.createWeatherApi()
 }
