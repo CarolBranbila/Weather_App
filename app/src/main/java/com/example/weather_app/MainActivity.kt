30b7848c228b795cf9d4d2149804cb23174e0a47
Carol Branbila <carolbranbila@hotmail.com>
2026-03-06 16:30:42 +0000

    MainNavGraph

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.weather_app.ui.theme.Weather_AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,3 +19,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
